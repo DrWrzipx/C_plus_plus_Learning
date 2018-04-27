f3f0673ffa28b11604f7821fe1884f6069a50d0c
DrWrzipx <matic.drobez@gmail.com>
2018-04-27 13:04:41 +0200

    Add some changes in Flow project

--- Flow/Flow.cpp
+++ Flow/Flow.cpp
@@ -4,27 +4,41 @@ int main()
 {
 	int j;
 	int i;
+	char f = 'a';
+	bool keepgoing = true;
+	int answer;
 
-	std::cout << "Please insert first number" << std::endl;
-	std::cin >> j;
-	std::cout << "Please insert second number" << std::endl;
-	std::cin >> i;
-
-
-	if (i < j)
+	while(keepgoing == true)
 	{
-		std::cout << "Second number is greater than first." << std::endl;
-	}
 
-	else if(i > j)
-	{
-		std::cout << "First number is greater than second." << std::endl;
-	}
+		std::cout << "Please insert first number" << std::endl;
+		std::cin >> j;
+		std::cout << "Please insert second number" << std::endl;
+		std::cin >> i;
+
+
+		if (i < j)
+		{
+			std::cout << "First number is greater than second number." << std::endl;
+		}
+
+		else if(i > j)
+		{
+			std::cout << "Second number is greater than first number." << std::endl;
+		}
 	
-	else
-	{
-		std::cout << "First and second numbers are equal." << std::endl;
-	}
+		else
+		{
+			std::cout << "First and second numbers are equal." << std::endl;
+		}
 
+		std::cout << "Compare another ? 0 for no: ";
+		std::cin >> answer;
+
+		if (answer == 0)
+			keepgoing = false;
+		else
+			keepgoing = true;
+	}
 	return 0;
 }
