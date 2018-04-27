7dc1fe9ee39f219dae8a44aaa612398822d5db70
DrWrzipx <matic.drobez@gmail.com>
2018-04-27 13:13:20 +0200

    Add for loop in flow

--- Flow/Flow.cpp
+++ Flow/Flow.cpp
@@ -8,37 +8,44 @@ int main()
 	bool keepgoing = true;
 	int answer;
 
-	while(keepgoing == true)
+	for(int loop = 0; loop < 10; loop++)
 	{
+		//keepgoing = true;
 
-		std::cout << "Please insert first number" << std::endl;
-		std::cin >> j;
-		std::cout << "Please insert second number" << std::endl;
-		std::cin >> i;
+		std::cout << loop << " " << std::endl;
 
-
-		if (i < j)
+		while(keepgoing == true)
 		{
-			std::cout << "First number is greater than second number." << std::endl;
-		}
 
-		else if(i > j)
-		{
-			std::cout << "Second number is greater than first number." << std::endl;
-		}
+			std::cout << "Please insert first number" << std::endl;
+			std::cin >> j;
+			std::cout << "Please insert second number" << std::endl;
+			std::cin >> i;
+
+
+			if (i < j)
+			{
+				std::cout << "First number is greater than second number." << std::endl;
+			}
+
+			else if(i > j)
+			{
+				std::cout << "Second number is greater than first number." << std::endl;
+			}
 	
-		else
-		{
-			std::cout << "First and second numbers are equal." << std::endl;
+			else
+			{
+				std::cout << "First and second numbers are equal." << std::endl;
+			}
+
+			std::cout << "Compare another ? 0 for no: ";
+			std::cin >> answer;
+
+			if (answer == 0)
+				keepgoing = false;
+			else
+				keepgoing = true;
 		}
-
-		std::cout << "Compare another ? 0 for no: ";
-		std::cin >> answer;
-
-		if (answer == 0)
-			keepgoing = false;
-		else
-			keepgoing = true;
 	}
 	return 0;
 }
