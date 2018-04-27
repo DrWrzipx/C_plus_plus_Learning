19d87cb8da1feec0de2eaadb029177740490cfa7
DrWrzipx <matic.drobez@gmail.com>
2018-04-27 12:41:10 +0200

    Add some changes to TutorialProject

--- TutorialProject/main.cpp
+++ TutorialProject/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <math.h>
 
 int main()
 {
 	std::cout << "Hello !";
 	std::cout << std::endl;
-	std::cout << 2 + 2;
+	std::cout << 2 + 2 << std::endl;
+
+	float i;
+	i = 4.5;
+	std::cout << i << std::endl;
+
+	float j;
+	j = 9/5;
+	std::cout << j << std::endl;
+	
 	return 0;
 }
